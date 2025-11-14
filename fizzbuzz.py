b98494cd1189ef00ddf5f10a13db7fa599d0728b
sungyong88-hub <sungyong88@gmail.com>
2025-11-14 10:25:26 +0900

    feat:Do fizzbuzz with 1 if Statement

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,6 @@
 for i in range(1, 15+1):
-   if i%15 == 0:
-     print('fizzbuzz')
-   elif i%5 == 0:
-     print('buzz')
-   elif i%3 == 0:
-     print('fizz')
-   else:
-      print(i)
+   if i%3 == 0 or i%5 ==0 :
+     print('fizz'*(i%==0) + 'buzz'*(i%5==0))
+      else
+        print(i)
 
